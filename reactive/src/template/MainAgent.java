2e26f4aa82e8f5d0f8d9138505dd60a10956b4cc
Eddie Wang <eddieshengyuwang@gmail.com>
2018-10-09 22:39:36 +0200

    a2 done

--- reactive/src/template/MainAgent.java
+++ reactive/src/template/MainAgent.java
@@ -1,10 +1,189 @@
 package template;
 
-public class MainAgent {
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hi");
+import logist.simulation.Vehicle;
+import logist.agent.Agent;
+import logist.behavior.ReactiveBehavior;
+import logist.plan.Action;
+import logist.plan.Action.Move;
+import logist.plan.Action.Pickup;
+import logist.task.Task;
+import logist.task.TaskDistribution;
+import logist.topology.Topology;
+import logist.topology.Topology.City;
+
+ public class MainAgent implements ReactiveBehavior {
+	private int numActions;
+	private Agent myAgent;
+	
+	// Map current state with next best action (represented by City)
+	private HashMap <State, City> actionTable; 
+	
+	// Map current state with best values for each action
+	private HashMap <State, Double> VTable; 
+	
+ 	@Override
+	public void setup(Topology topology, TaskDistribution td, Agent agent) {
+ 		// Reads the discount factor from the agents.xml file.
+		// If the property is not present it defaults to 0.95
+		Double discount = agent.readProperty("discount-factor", Double.class,
+				0.95);
+		
+		Vehicle vehicle = agent.vehicles().get(0);
+		
+		// All possible condition for tasks (no task is represented by NULL)
+		List<City> tasks = new ArrayList<Topology.City>(topology.cities());		
+		tasks.add(null);  
+		
+		actionTable = new HashMap<State, City>();
+		VTable = new HashMap<State, Double>();
+		
+		boolean goodEnough = false;
+		
+		do {
+			goodEnough = false;			
+			
+			for (City city: topology.cities()) {
+				for (City taskCity: tasks) {
+					
+					if (taskCity != null && city.id == taskCity.id) continue;
+					
+					// State S
+					State currentState = new State(city, taskCity);
+					
+					List<City> validActions = getValidDestFrom(currentState);
+					
+					double maxQ = Double.NEGATIVE_INFINITY;
+					City nextAction = null;
+					
+					for (City action: validActions) {
+					
+						// R(s,a) 
+						double qValue = getReward(currentState, action, vehicle, td);
+						
+						// Sum of T(s, a, s') for all s' in tasks
+						
+						City nextStateCity = action;
+						
+						for (City nextTaskCity: tasks) {
+							
+							if (nextTaskCity != null && nextStateCity.id == nextTaskCity.id) continue;
+							
+							// State S'
+							State nextState = new State(nextStateCity, nextTaskCity);
+							
+							double probablity = getProbability(currentState, nextState, action, td);
+							
+							qValue += discount * probablity * getVTable(nextState);
+						}
+						
+						// Pick max_a Q(s, a)  for each action
+						if (qValue > maxQ) {
+							maxQ = qValue;
+							nextAction = action;
+						}
+					}
+					
+					// V(s, a) = max_a Q(s, a)
+					if (getVTable(currentState) != maxQ) {
+						setVTable(currentState, maxQ);
+						setActionTable(currentState, nextAction);
+						goodEnough = true;
+					}
+				}
+			}
+		} while (goodEnough);
+		
+		this.numActions = 0;
+		this.myAgent = agent;
 	}
-
+ 	
+	@Override
+	public Action act(Vehicle vehicle, Task availableTask) {
+		Action action;
+		City currentCity = vehicle.getCurrentCity();
+		City deliveryCity = null;
+		
+		if (availableTask != null) {
+			deliveryCity = availableTask.deliveryCity;
+		}
+		
+		State state = new State(currentCity, deliveryCity);
+		
+		City nextAction = getActionTable(state);
+		 
+		if(nextAction.equals(deliveryCity)) {
+			action = new Pickup(availableTask);
+		} else {
+			action = new Move(nextAction);
+		}
+		
+		if (numActions >= 1) {
+			System.out.println("The total profit after "+numActions+" actions is "+myAgent.getTotalProfit()+" (average profit: "+(myAgent.getTotalProfit() / (double)numActions)+")");
+		}
+		numActions++;
+		
+		return action;
+	}
+	
+	private List<City> getValidDestFrom(State state) {
+		List<City> validDestCities = new ArrayList<Topology.City>(state.city.neighbors());
+		
+		if (state.taskCity != null) { // if delivery city is not null
+			validDestCities.add(state.taskCity);
+		}
+		
+		return validDestCities;
+	}
+	
+ 	
+	private double getReward(State state, City destCity, Vehicle vehicle, TaskDistribution td) {
+		
+		double reward = 0;
+		
+		if (state.taskCity != null && destCity.id == state.taskCity.id) {
+				reward = td.reward(state.city, destCity);
+		} 
+		
+		reward  -= state.city.distanceTo(destCity) * vehicle.costPerKm();
+		
+		return reward;
+	}
+	
+	private double getProbability(State fromState, State toState, City destCity, TaskDistribution td) {
+		
+		if ((fromState.city.id == destCity.id) ||
+				(fromState.city.id == toState.city.id) ||
+				(fromState.taskCity != null && fromState.city.id == fromState.taskCity.id)) {
+			return 0.0;
+		} else if (destCity.id != toState.city.id) {
+			return 0.0;
+		} else {
+			return td.probability(toState.city, toState.taskCity); 
+		}
+	}
+	
+	private double getVTable(State state) {
+		if (VTable.get(state) != null) {
+			return VTable.get(state);
+		} else {
+			return 0.0;
+		}
+	}
+	
+	private void setVTable(State state, double value) {
+		VTable.put(state, value);
+	}
+	
+	private City getActionTable(State state) {
+		return actionTable.get(state);
+	}
+	
+	private void setActionTable(State state, City action) {
+		actionTable.put(state, action);
+	}
+	
 }
--- reactive/src/template/State.java
+++ reactive/src/template/State.java
@@ -1,19 +1,45 @@
 package template;
-import logist.simulation.Vehicle;
-import java.util.ArrayList;
+
 import java.util.List;
-import logist.topology.Topology;
 import logist.topology.Topology.City;
 
 public class State {
 	public City city;
-	public List<City> neighbourCities;
-	public Vehicle vehicle;
+	public City taskCity;
 	
-	public State(City city, Vehicle vehicle) {
+	public State(City city, City taskCity) {
 		this.city = city;
-		this.neighbourCities = city.neighbors();
-		this.vehicle = vehicle;
-	}	
-	
+		this.taskCity = taskCity;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((taskCity == null) ? 0 : taskCity.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		State other = (State) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (taskCity == null) {
+			if (other.taskCity != null)
+				return false;
+		} else if (!taskCity.equals(other.taskCity))
+			return false;
+		return true;
+	}		
 }
